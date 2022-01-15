26e98dc47440108e4c6b1a2efe91070cdcc47717
T-unity <takadanaritoshi@gmail.com>
2022-01-15 22:21:59 +0900

    関連動画をブロック

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+  window.setTimeout( () => {
+    const target = document.getElementById('secondary')
+    console.log(target)
+    target.style.display = 'none'
+  }, 10000)
+
+
+}
